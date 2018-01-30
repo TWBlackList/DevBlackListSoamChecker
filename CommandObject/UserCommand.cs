54452c031397d65a49597c5d44f809c05b85e685
OwO <OwO@OmO.tw>
2018-01-30 23:02:50 +0800

    fix typo

--- CommandObject/UserCommand.cs
+++ CommandObject/UserCommand.cs
@@ -4,7 +4,7 @@ using ReimuAPI.ReimuBase.TgData;
 namespace CNBlackListSoamChecker.CommandObject {
     internal class UserCommand {
         internal bool User(TgMessage RawMessage){
-            TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetSendUser().id.ToString(),RawMessage.message_id);
+            TgApi.getDefaultApiConnection().sendMessage(RawMessage.GetMessageChatInfo().id,RawMessage.GetSendUser().id.ToString(),RawMessage.message_id);
             return true;
         }
     }
