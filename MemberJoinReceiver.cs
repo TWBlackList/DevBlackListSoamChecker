51331d596df79ed3a72cc0739049de362ff1b9be
OwO <OwO@OmO.tw>
2018-02-16 16:25:23 +0800

    [reportGroup] fix using

--- MemberJoinReceiver.cs
+++ MemberJoinReceiver.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using ReimuAPI.ReimuBase;
 using ReimuAPI.ReimuBase.Interfaces;
 using ReimuAPI.ReimuBase.TgData;
-using TWBlackListSoamChecker.DbManager;
+using CNBlackListSoamChecker.DbManager;
 
 namespace CNBlackListSoamChecker
 {
