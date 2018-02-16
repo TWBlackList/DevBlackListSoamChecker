e38685608ad50e181923d4f551239ee6bca94f7d
OwO <OwO@OmO.tw>
2018-02-16 16:07:25 +0800

    [memberJoinMsg] fix escape

--- MemberJoinReceiver.cs
+++ MemberJoinReceiver.cs
@@ -79,7 +79,7 @@ namespace CNBlackListSoamChecker
                 if (banUser.Ban == 0)
                 {
                     string resultmsg = "這位使用者被封鎖了";
-                    resultmsg += "，原因 : \n" + banUser.Reason + "\nID : " + JoinedUser.id;
+                    resultmsg += "，原因 : \n" + RAPI.escapeMarkdown(banUser.Reason) + "\nID : " + JoinedUser.id;
                     if (banUser.ChannelMessageID != 0)
                         resultmsg += "\n參考 : https://t.me/" + Temp.MainChannelName + "/" + banUser.ChannelMessageID ;
                     TgApi.getDefaultApiConnection().sendMessage(
