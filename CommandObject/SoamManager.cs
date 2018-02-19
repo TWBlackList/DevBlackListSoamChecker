c8a25d95a76edab4a4c88c82a50da921a119aedf
OwO <OwO@OmO.tw>
2018-02-19 13:36:07 +0800

    [soam] fix blacklist username

--- CommandObject/SoamManager.cs
+++ CommandObject/SoamManager.cs
@@ -230,7 +230,7 @@ namespace DevBlackListSoamChecker.CommandObject
             GroupCfg gc = Temp.GetDatabaseManager().GetGroupConfig(message.chat.id);
             TgApi.getDefaultApiConnection().sendMessage(
                 message.chat.id,
-                "BlackList" + TgApi.getDefaultApiConnection().getMe().username + " : " + (gc.BlackList == 0) + "\n" +
+                "BlackList (By @" + TgApi.getDefaultApiConnection().getMe().username + ") : " + (gc.BlackList == 0) + "\n" +
                 "AutoKick : " + (gc.AutoKick == 0) + "\n" +
                 "AntiBot : " + (gc.AntiBot == 0) + "\n" +
                 "AntiHalal : " + (gc.AntiHalal == 0) + "\n" +
