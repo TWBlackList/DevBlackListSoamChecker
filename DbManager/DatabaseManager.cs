4556633c10f5bde17a1890f68679baaccb384bd8
OwO <OwO@OmO.tw>
2018-04-04 00:28:27 +0800

    fix typo

--- DbManager/DatabaseManager.cs
+++ DbManager/DatabaseManager.cs
@@ -51,11 +51,11 @@ namespace DevBlackListSoamChecker.DbManager
             int ReasonID = 0;
             int ChannelReasonID = 0;
             System.Console.WriteLine("Checking ChatID Message");
-            if (Temp.ReasonChannelID != 0 && ChatID != 0 && MessageID != 0 &&)
+            if (Temp.ReasonChannelID != 0 && ChatID != 0 && MessageID != 0)
             {
                 System.Console.WriteLine("Forward Message");
                 SendMessageResult forward = TgApi.getDefaultApiConnection()
-                    .forwardMessage(Temp.ReasonChannelID, ChatID, MessageID)
+                    .forwardMessage(Temp.ReasonChannelID, ChatID, MessageID);
                 if (forward.result.ok)
                 {
                     ReasonID = TgApi.getDefaultApiConnection().forwardMessage(Temp.ReasonChannelID, ChatID, MessageID)
