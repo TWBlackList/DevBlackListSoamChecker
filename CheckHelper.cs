7d6be20068a34ab743e02cefa2896ca6eafd8b3e
OwO <OwO@OmO.tw>
2018-04-05 21:54:35 +0800

    fix

--- CheckHelper.cs
+++ CheckHelper.cs
@@ -7,21 +7,17 @@ namespace DevBlackListSoamChecker
     {
         public bool CheckAdminInReportGroup(long ChatID)
         {
-            System.Console.WriteLine("CheckAdminInReportGroup Called. ChatID" + ChatID.ToString());
             if (Temp.ReportGroupID != 0)
             {
                 foreach (long i in Temp.adminInReport)
                     if (i == ChatID)
                         return true;
-                System.Console.WriteLine("Not in temp list");
                 
                 bool status = false;
                 GroupUserInfo[] admins = TgApi.getDefaultApiConnection().getChatAdministrators(ChatID,true);
-                System.Console.WriteLine("get admins");
                 foreach (var admin in admins)
                 {
-                    var result = TgApi.getDefaultApiConnection().getChatMember(ChatID, admin.user.id);
-                    System.Console.WriteLine("result ok " + result.ok.ToString() + " status" + result.result.status);
+                    var result = TgApi.getDefaultApiConnection().getChatMember(Temp.ReportGroupID, admin.user.id);
                     if (result.ok)
                         if(result.result.status != "left")
                         {
