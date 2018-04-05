cc367e4b90e1fb4340ceb6458d5733d97a69b6ab
OwO <OwO@OmO.tw>
2018-04-05 21:25:25 +0800

    fix

--- SpamMessageDeleter.cs
+++ SpamMessageDeleter.cs
@@ -34,7 +34,7 @@ namespace DevBlackListSoamChecker
                 return new CallbackMessage();
             }
 
-            if (CheckInReportGroup.CheckAdminInReportGroup(BaseMessage.GetMessageChatInfo().id) == false)
+            if (CheckAdminInReportGroup(BaseMessage.GetMessageChatInfo().id) == false)
             {
                 new Thread(delegate()
                 {
@@ -555,6 +555,40 @@ namespace DevBlackListSoamChecker
             {
                 CallAdmin_SendMessage(msg, string.Join("", temp), step);
                 temp.Clear();
+            }
+        }
+        
+        private bool CheckAdminInReportGroup(long ChatID)
+        {
+            if (Temp.ReportGroupID != 0)
+            {
+                if (Temp.adminInReport.Contains(ChatID))
+                    return true;
+                
+                bool status = false;
+                GroupUserInfo[] admins = TgApi.getDefaultApiConnection().getChatAdministrators(ChatID,true);
+                foreach (var admin in admins)
+                {
+                    System.Console.WriteLine("getting member chatid" + ChatID + " userid" + admin.user.id);
+                    var result = TgApi.getDefaultApiConnection().getChatMember(ChatID, admin.user.id);
+                    System.Console.WriteLine("ok" + result.ok.ToString() + " status" + result.result.status);
+                    if (result.ok)
+                        if(result.result.status != "left")
+                        {
+                            status = true;
+                            break;
+                        }
+                }
+
+                if (status)
+                    Temp.adminInReport.Add(ChatID);
+
+                return status;
+
+            }
+            else
+            {
+                return true;
             }
         }
     }
