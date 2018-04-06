32e5a1e97b94117b6429709672b84483d05e76bd
OwO <OwO@OmO.tw>
2018-04-06 21:39:39 +0800

    [command] fix command

--- AdminCommand.cs
+++ AdminCommand.cs
@@ -24,6 +24,9 @@ namespace DevBlackListSoamChecker
                 if (!Temp.DisableBanList)
                     switch (Command)
                     {
+                        case "/groupadmin":
+                            new GetAdmins().GetGroupAdmins(RawMessage);
+                            throw new StopProcessException();
                         case "/getspampoints":
                             new SpamStringManager().GetSpamPoints(RawMessage);
                             throw new StopProcessException();
@@ -62,9 +65,6 @@ namespace DevBlackListSoamChecker
                         }
                     switch (Command)
                     {
-                        case "/groupadmin":
-                            new GetAdmins().GetGroupAdmins(RawMessage);
-                            throw new StopProcessException();
                         case "/points":
                             new SpamStringManager().GetSpamKeywords(RawMessage);
                             throw new StopProcessException();
